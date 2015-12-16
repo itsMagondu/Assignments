0bd68c8f37e5217e63fc6c7f3031469f53478221
Magondu <MagonduNjenga@gmail.com>
2015-12-16 19:33:12 +0300

    Added html code. Not workong

--- cdt504/webservice/client.php
+++ cdt504/webservice/client.php
@@ -1,6 +1,6 @@
 <?php
 require_once "lib/nusoap.php";
- 
+
 //$client = new nusoap_client("http://localhost/web/cdt504/webservice/server.php");
 //$error  = $client->getError();
 
@@ -17,7 +17,7 @@ require_once "lib/nusoap.php";
 //if ($error) {
 //    echo "<h2>Constructor error</h2><pre>" . $error . "</pre>";
 //}
- 
+
 //if ($method === "fibList")
 //{
 //$result = $client->call("math.fibList", array("num" => $num));
@@ -55,81 +55,79 @@ require_once "lib/nusoap.php";
     <meta name="description" content="This is a web interface that takes advantage of the fibonnaci web service">
     <meta name="author" content="Magondu Njenga">
 
-    <link href="scripts/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="scripts/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <style type="text/css">
+    body {
+        padding-left: 11em;
+        font-family: Georgia, "Times New Roman", Times, serif;
+        color: #7d4627;
+        background-color: #c9c9c9;
+    }
 
+    h1 {
+        font-family: Helvetica, Geneva, Arial, SunSans-Regular, sans-serif;
+    }  
+    </style>
 
-    <!-- Icons -->
-    <link href="scripts/icons/general/stylesheets/general_foundicons.css" media="screen" rel="stylesheet" type="text/css" />  
-    <link href="scripts/icons/social/stylesheets/social_foundicons.css" media="screen" rel="stylesheet" type="text/css" />
-
-    <link rel="stylesheet" href="scripts/fontawesome/css/font-awesome.min.css">
-
-
-    <link href="scripts/wookmark/css/style.css" rel="stylesheet" type="text/css" /> <link href="scripts/yoxview/yoxview.css" rel="stylesheet" type="text/css" />
-    <link href="http://fonts.googleapis.com/css?family=Syncopate" rel="stylesheet" type="text/css">
-
-
-    <link href="styles/custom.css" rel="stylesheet" type="text/css" />
-     <script type="text/javascript">
+    <script type="text/javascript">
     function validateandget()
- {
-    
-    var fibnumber=document.enq.number.value;
-    var number_exp=/^[0-9]/;
-    if(fibnumber=='')
     {
-        alert("Number Should Not Be Empty!");
-        document.enq.number.focus();
-        return false;
-    }
-    else if(!fibnumber.match(number_exp))
-    {
-        alert("Invalid Number!");
-        document.enq.number.focus();
-        return false;
-    }
-    else 
-    {
-        //make a js call to the server and get results
-    }    
+
+        var fibnumber=document.fibform.fibnum.value;
+        var choice =document.fibform.fibtye.value;
+        var number_exp=/^[0-9]/;
+        if(fibnumber=='')
+        {
+            alert("Number Should Not Be Empty!");
+            document.enq.number.focus();
+            return false;
+        }
+        else if(!fibnumber.match(number_exp))
+        {
+            alert("Invalid Number!");
+            document.enq.number.focus();
+            return false;
+        }
+        else 
+        {
+            <?php 
+            $client = new nusoap_client("http://localhost/msc/cdt504/webservice/server.php");
+            $error  = $client->getError();
+
+            if (choice === "list")
+            {
+                $result = $client->call("math.fibList", array("num" => $num));
+            }
+            elseif (choice === "num")
+            {
+                $result = $client->call("math.fibNum", array("num" => $num));
+            }
+            else
+            {
+                //alert("An error occured");
+            }
+
+            ?>
+        }    
 
     }
     return true;
- }
+}
 
-    </script>
+</script>
 </head>
-<body id="pagebody">
+<body>
     <div id="divBoxed" class="container">
-     <div class="contentArea">
-          <div class="divPanel notop page-content">
-             <div class="row-fluid">
-                <div class="span8" id="divMain">
+        <h1>Welcome to the Fibbonaci Page</h1>
 
-                    <h1>Welcome to the Fibbonaci Page</h1>
-
-                    <form name="enq" method="get" action="/web/cdt504/webservice/server.php" onsubmit="return valideandget();">
-  <fieldset>
-    
-    <input type="number" name="number" id="number" value=""  class="input-block-level" placeholder="Enter Number" />
-    <div class="actions">
-    <input type="submit" value="Send Your Message" name="submit" id="submitButton" class="btn btn-info pull-right" title="Click here to submit your message!" />
+        <form name="fibform" method="get" onsubmit="return valideandget();">
+            Enter Number: <input type="numer" name="fibnum"><br><br>
+            <input type="radio" name="fibnumber" value="num" checked>Number
+            <input type="radio" name="fibtype" value="list">List <br><br>
+            <input type="submit" value="Submit">
+        </form>             
     </div>
-    
-    </fieldset>
-    <table>
-    </table>
-</form>             
-                </div>
-            </div>
-          </div>
-     </div>
-    </div>
-
-   
 </body>
- 
+
 <?php 
 // show soap request and response
 //echo "<h2>Request</h2>";
